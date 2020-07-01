0a26457493deb40f3ae91309dbe8fb9271996c49
d.komov <d.komov@escrow.com.ua>
2020-07-01 17:08:35 +0300

    create app skeleton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './scss/index.scss';
-console.log('Working');
-const superArr = [1, [2], [3]];
-superArr.flat(Infinity);
+import {Excel} from '@/components/excel/Excel';
+
+const excel = new Excel('#app', {
+  components: []
+});
+console.log(excel);
